b0c64bc7a79ca576118adb9e9dd86cf3b52d74be
soto <kiso.soto@gmail.com>
2025-07-18 21:33:36 +0900

    ダミーデータの作成

--- src/database/seeders/RestsTableSeeder.php
+++ src/database/seeders/RestsTableSeeder.php
@@ -20,5 +20,61 @@ class RestsTableSeeder extends Seeder
             'end' => '13:00',
         ];
         Rest::create($param);
+
+        $param = [
+            'record_id' => 2,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 3,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 4,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 5,
+            'start' => '15:00',
+            'end' => '15:30',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 5,
+            'start' => '19:00',
+            'end' => '20:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 6,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 7,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
+
+        $param = [
+            'record_id' => 8,
+            'start' => '12:00',
+            'end' => '13:00',
+        ];
+        Rest::create($param);
     }
 }
